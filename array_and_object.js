d218595683a963727936dc40e0dc00e25fb953bb
faiz-dev <68036226+faiz-dev@users.noreply.github.com>
2024-10-31 11:41:50 +0700

    fitur hapus

--- array_and_object.js
+++ array_and_object.js
@@ -1,15 +1,64 @@
-// ARRAY
-const siswa = ["Rosa",  "Ambar", "Rizki", "Mila", "Bagas"]
-const jenKel = ["P", "L", "P", "P", "L"]
-const umur = ["17", "16", "16", "18", "15"]
-const wfavorit = ["kuning", "merah", "ungu", "biru", "coklat"]
-
 // OBJECT
-const siswa1 = {
-  nama: "Rosa",
-  jenKel: "P",
-  umur: "17",
-  wfavorit: "kuning",
-  seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
+const daftarSiswa = [
+  {
+    nama: "Rosa",
+    jenKel: "P",
+    umur: "17",
+    wfavorit: "kuning",
+    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
+  },
+  {
+    nama: "Rizki",
+    jenKel: "L",
+    umur: "16",
+    wfavorit: "merah",
+    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
+  }
+]
+
+
+// CREATE, READ, UPDATE, DELETE
+
+// READ
+// arrow function
+const tampilkanSiswa = () => {
+  for (let idx in daftarSiswa) {
+    console.log(`${parseInt(idx) + 1}. ${daftarSiswa[idx].nama} seorang ${daftarSiswa[idx].jenKel} suka warna ${daftarSiswa[idx].wfavorit} berumur ${daftarSiswa[idx].umur}`)
+  }
 }
+
+// CREATE
+const tambahSiswa = () => {
+  const nama = prompt('Masukkan Nama')
+  const jenKel = prompt('Masukkan Jenis Kelamin')
+  const umur = prompt('Masukkan Umur')
+  const warna = prompt('Masukkan Warna Favorit')
+
+  const siswaBaru = {
+    nama: nama,
+    jenKel: jenKel,
+    umur: umur,
+    wfavorit: warna,
+    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
+  }
+
+  daftarSiswa.push(siswaBaru)
+}
+
+
+const cariIndex = (nama) => {
+  // tampilkan index jika nama siswa === nama
+  for(let i = 0; i<daftarSiswa.length; i++) {
+    if (daftarSiswa[i].nama == nama) {
+      return i
+    }
+  }
+}
+
+const hapusSiswa = (target) => {
+  const indexDihapus = cariIndex(target)
+  // menghapus element dari dalam array
+  daftarSiswa.splice(indexDihapus, 1)
+}
+
 

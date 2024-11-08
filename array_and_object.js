926c9bfc751e453d2a2bd06f93c3ea65394e8dc5
BayuSuryadinata <bayusurya380@gmail.com>
2024-11-08 11:04:47 +0700

    iya

--- array_and_object.js
+++ array_and_object.js
@@ -1,85 +1,46 @@
-// OBJECT
-const daftarSiswa = [
+// Object dalam array
+
+const daftarSiswa = 
+[
   {
-    nama: "Rosa",
-    jenKel: "P",
-    umur: "17",
-    wfavorit: "kuning",
-    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
-  },
-  {
-    nama: "Rizki",
-    jenKel: "L",
-    umur: "16",
-    wfavorit: "merah",
-    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
+    nama : "Bayu",
+    jenKel : "L",
+    umur : 17,
+    wfavorit : "Merah",
+    seragam : ["Osis", "Batik"]
   }
 ]
 
+const tampilkanSiswa = () =>
+{
+  const tblSiswa = document.getElementById("tblSiswa");
 
-// CREATE, READ, UPDATE, DELETE
+  tblSiswa.innerHTML = 
+  `
+  <tr>
+    <th>No</th>
+    <th>Nama</th>
+    <th>Jenkel</th>
+    <th>Umur</th>
+    <th>Warna</th>
+    <th>Edit</th>
+    <th>Hapus</th>
+  </tr>
+  `
 
-// READ
-// arrow function
-const tampilkanSiswa = () => {
-  // mengakses dom
-  const tblSiswa = document.getElementById('tblSiswa')
-  tblSiswa.innerHTML = '<tr><th>No</th><th>Nama</th><th>JenKel</th><th>Umur</th><th>Warna</th></tr>'
-
-  for (let idx in daftarSiswa) {
-    console.log(`${parseInt(idx) + 1}. ${daftarSiswa[idx].nama} seorang ${daftarSiswa[idx].jenKel} suka warna ${daftarSiswa[idx].wfavorit} berumur ${daftarSiswa[idx].umur}`)
-
-
-    // menambah isinya 
-    tblSiswa.innerHTML += `<tr><td>${parseInt(idx) + 1}</td><td>${daftarSiswa[idx].nama}</td><td>${daftarSiswa[idx].jenKel}</td><td>${daftarSiswa[idx].umur}</td><td>${daftarSiswa[idx].wfavorit}</td></tr>`
+  for (let idx in daftarSiswa)
+  {
+    tblSiswa.innerHTML += `
+    <tr>
+      <td>${parseInt(idx)+1}</td>
+      <td>${daftarSiswa[idx].nama}</td>
+      <td>${daftarSiswa[idx].jenKel} </td>
+      <td>${daftarSiswa[idx].umur} </td>
+      <td>${daftarSiswa[idx].wfavorit} </td>
+      <td><button type="button" class="btn btn-warning">Edit</button></td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+    `
+    
   }
-}
-
-// CREATE
-const tambahSiswa = () => {
-  const nama = document.getElementById('txtNama').value
-  const jenKel = document.getElementById('jenKel').value
-  const umur = document.getElementById('txtUmur').value
-  const warna = document.getElementById('warna').value
-
-  const siswaBaru = {
-    nama: nama,
-    jenKel: jenKel,
-    umur: umur,
-    wfavorit: warna,
-    seragam: ['osis', 'identitas', 'wearpack', 'pramuka', 'olahraga']
-  }
-
-  daftarSiswa.push(siswaBaru)
-
-  document.getElementById('txtNama').value = ""
-
-  tampilkanSiswa()
-}
-
-
-const cariIndex = (nama) => {
-  // tampilkan index jika nama siswa === nama
-  for(let i = 0; i<daftarSiswa.length; i++) {
-    if (daftarSiswa[i].nama == nama) {
-      return i
-    }
-  }
-}
-
-const hapusSiswa = (target) => {
-  const indexDihapus = cariIndex(target)
-  // menghapus element dari dalam array
-  daftarSiswa.splice(indexDihapus, 1)
-}
-
-const editSiswa = (target) => {
-  const indexEdit = cariIndex(target)
-
-  daftarSiswa[indexEdit] = {
-    nama: namaBaru,
-    jenKel: jenKelBaru,
-    wfavorit: wfavoritBaru,
-    umur: umurBaru
-  }
-}
+}                                                                                 
